2495adfdd332d74d15500ff36b14e7524a3bea92
Johanna Gfüllner <s1060922@stud.sbg.ac.at>
2021-05-01 09:21:41 +0200

    Reduced density

--- prog/create_scene_patches.m
+++ prog/create_scene_patches.m
@@ -30,7 +30,7 @@ P.scene_context = 1 ;                % 0=patch only, 1=patch in context
 P.context_color = [57 255 20] ;      % Color used to highlight patch
 P.context_scale = 1 ;              % Scale down context scene size
 P.patch_diameter = [42 91] ;        % Patch diameter (px) [fine coarse]
-P.patch_density = [95 35] ;        % Patch density (number) [fine coarse]
+P.patch_density = [35 10] ;        % Patch density (number) [fine coarse]
 P.catch_trials = 1 ;                 % Include low meaning catch trials
                                      % *Only for P.scene_context=0 case
 
